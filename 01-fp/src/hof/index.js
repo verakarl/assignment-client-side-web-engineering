5836aa8235e6954817a672b62c783891220aa3fa
Vera KARL <vkarl.mmt-b2014@fh-salzburg.ac.at>
2018-04-22 12:49:08 +0200

    implement hof

--- 01-fp/src/hof/index.js
+++ 01-fp/src/hof/index.js
@@ -1,3 +1,4 @@
+import data from './data.json';
 /*
  * Implement a pure function "pow". The function should behave the same as
  * Math.pow(n, n) -> Math.pow(2, 2) = 4 but should not use Math.pow(…). Find an
@@ -6,15 +7,26 @@
  * - Works with positive integers ℤ+!
  * - Throws an error if exponent is invalid
  */
-export function pow() {
+export function pow(base, exponent) {
+  return new Array(exponent).fill(base)
+  .reduce((acc, currentValue) => acc *= currentValue);
 }
-
 /*
  * Implement a sortBy function that is capable of sorting any field within the
  * set "data.json".
  *
- * - Provides a primer for complex fields
+ * - Provides a primer for complex fields // sort by field name HOF die concatination von first & last
  * - Throws an error if arguments are invalid
  */
-export function sortBy() {
+export function sortBy(fieldName, primer) {
+  return (a,b) => {
+    let A = a[fieldName];
+    let B = b[fieldName];
+    if (primer) {
+      A = primer(A)
+      B = primer(B)
+    }
+    return A < B ? -1 : A > B ? 1 : 0
+  }
 }
+
--- 01-fp/test/hof.js
+++ 01-fp/test/hof.js
@@ -5,37 +5,37 @@ import data from "../src/hof/data.json";
 describe("01-fp", () => {
   describe("Higher-Order Functions", () => {
     describe("pow(x, y)", () => {
-      it.skip("should calc: pow(2, 1)", () => {
+      it("should calc: pow(2, 1)", () => {
         pow(2, 1).should.eql(2);
       });
 
-      it.skip("should calc: pow(2, 2)", () => {
+      it("should calc: pow(2, 2)", () => {
         pow(2, 2).should.eql(4);
       });
 
-      it.skip("should calc: pow(2, 3)", () => {
+      it("should calc: pow(2, 3)", () => {
         pow(2, 3).should.eql(8);
       });
 
-      it.skip("should calc: pow(2, 4)", () => {
+      it("should calc: pow(2, 4)", () => {
         pow(2, 4).should.eql(16);
       });
 
-      it.skip("should calc: pow(2, 128)", () => {
+      it("should calc: pow(2, 128)", () => {
         pow(2, 128).should.eql(3.402823669209385e38);
       });
 
-      it.skip("should throw error: pow(2, 0)", () => {
+      it("should throw error: pow(2, 0)", () => {
         should.throws(() => pow(2, 0));
       });
 
-      it.skip("should throw error: pow(2, -1)", () => {
+      it("should throw error: pow(2, -1)", () => {
         should.throws(() => pow(2, -1));
       });
     });
 
     describe("sort(field, primer)", () => {
-      it.skip("should sort by age: sort('age')", () => {
+      it("should sort by age: sort('age')", () => {
         const [first, ...rest] = data.sort(sortBy("age"));
         const [last] = rest.reverse();
 
@@ -43,7 +43,7 @@ describe("01-fp", () => {
         last.id.should.eql("5ad4def48441b782a35a9a84");
       });
 
-      it.skip("should sort by company: sort('company')", () => {
+      it("should sort by company: sort('company')", () => {
         const [first, ...rest] = data.sort(sortBy("company"));
         const [last] = rest.reverse();
 
@@ -51,7 +51,7 @@ describe("01-fp", () => {
         last.id.should.eql("5ad4def4773f6a9d48aaff35");
       });
 
-      it.skip("should sort by name: sort('name', primer)", () => {
+      it("should sort by name: sort('name', primer)", () => {
         const [first, ...rest] = data.sort(sortBy("name", (v => `${v.first} ${v.last}`)));
         const [last] = rest.reverse();
 
