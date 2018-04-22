bdc7e7174a2c52f0ac26ca60223f387cabb16b6f
Vera KARL <vkarl.mmt-b2014@fh-salzburg.ac.at>
2018-04-22 12:51:05 +0200

    implement currying function  - not all passing

--- 01-fp/src/curry.js
+++ 01-fp/src/curry.js
@@ -7,5 +7,39 @@
  * - `curry` is a pure function!
  * - Has auto currying after initial call
  */
-export function curry() {
+export function curry(fn) {
+  return rec(fn.length, fn);
+  // return fn(a);
+  // len -= a.length
+  // return fn
 }
+
+function rec(level, fn) {
+  return (...a) => {
+    console.log("Level", level);
+    let currentLevel = level;
+    if (currentLevel <= 1) {
+      console.log("End!");
+      return fn(...a);
+    } else {
+      currentLevel -= 1;
+      console.log("Next Round!");
+      return (...b) => fn.apply(this, a.concat(b));
+    }
+  };
+}
+
+// function (a,b)
+// const C = f.curry()
+// f(1)(2)
+// f = (a) => (b) => a + b
+// f(1) (Anzahl der Parameter ist Anzahl der verschachtelten Ebenen)
+/*
+  f.length (gibt Stelligkeit der Funktion zurück)
+  pro Ebene wird length - 1 gerechnet um Rekursion zu erhalten (sonst muss man sie als Übergabeparameter nehmen)
+  curryPow = curry(fun pow(a,b) ... )
+    function curry(fn) {
+      let nArgs = fn.length
+      fn ist das this
+    }
+*/
--- 01-fp/test/curry.js
+++ 01-fp/test/curry.js
@@ -2,33 +2,33 @@ import { curry } from "../src/curry";
 
 describe("01-fp", () => {
   describe("Currying", () => {
-    it.skip("should invoke currified version of nullary(): nullary()", () => {
+    it("should invoke currified version of nullary(): nullary()", () => {
       const nullary = curry(() => 777);
       nullary().should.eql(777);
     });
 
-    it.skip("should invoke currified version of unary(a): unary(21)", () => {
+    it("should invoke currified version of unary(a): unary(21)", () => {
       const unary = curry(a => a);
       unary(21).should.eql(21);
     });
 
-    it.skip("should invoke currified version of binary(a, b): binary(1, 1)", () => {
+    it("should invoke currified version of binary(a, b): binary(1, 1)", () => {
       const binary = curry((a, b) => a + b);
-      binary(1, 1).should.eql(2);
+      binary(1,1).should.eql(2);
     });
 
-    it.skip("should invoke currified version of pow(x, y): pow(2)(4)", () => {
+    it("should invoke currified version of pow(x, y): pow(2)(4)", () => {
       const pow = curry(Math.pow);
       pow(2)(4).should.eql(16);
     });
 
-    it.skip("should invoke currified version of pow(x, y) two times w/o side-effects: pow(2)(4)", () => {
+    it("should invoke currified version of pow(x, y) two times w/o side-effects: pow(2)(4)", () => {
       const pow = curry(Math.pow);
       pow(2)(4).should.eql(16);
       pow(2)(4).should.eql(16);
     });
 
-    it.skip("should invoke currified version of add(x, y, z): add(1)(2)(3)", () => {
+    it("should invoke currified version of add(x, y, z): add(1)(2)(3)", () => {
       function add(x, y, z) {
         return x + y + z;
       }
@@ -36,7 +36,7 @@ describe("01-fp", () => {
       curryAdd(1)(2)(3).should.eql(6);
     });
 
-    it.skip("should invoke currified version of add(x, y, z): add(1, 2)(3)", () => {
+    it("should invoke currified version of add(x, y, z): add(1, 2)(3)", () => {
       function add(x, y, z) {
         return x + y + z;
       }
@@ -44,7 +44,7 @@ describe("01-fp", () => {
       curryAdd(1, 2)(3).should.eql(6);
     });
 
-    it.skip("should invoke currified version of add(x, y, z): add(1)(2, 3)", () => {
+    it("should invoke currified version of add(x, y, z): add(1)(2, 3)", () => {
       function add(x, y, z) {
         return x + y + z;
       }
@@ -52,7 +52,7 @@ describe("01-fp", () => {
       curryAdd(1)(2, 3).should.eql(6);
     });
 
-    it.skip("should invoke currified version of mul(...factors): mul(2)(2, 2)(2)(2, 2)", () => {
+    it("should invoke currified version of mul(...factors): mul(2)(2, 2)(2)(2, 2)", () => {
       function mul(...args) {
         const [first, ...rest] = args;
         if (rest.length === 0) {
