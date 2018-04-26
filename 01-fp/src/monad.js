62de5ec1e74dcda3e565f6d1c48ee8dbf7b092f7
Vera KARL <vkarl.mmt-b2014@fh-salzburg.ac.at>
2018-04-26 11:12:23 +0200

    first time of monading

--- 01-fp/src/monad.js
+++ 01-fp/src/monad.js
@@ -3,7 +3,7 @@
  *
  * - Create a monad
  * - 1. type constructor: Create a constructor for a monad
- * - 2. unit function: Wrap a value of given type into a monad
+ * - 2. unit function: Wrap a value of given type into a monad // lift = constructor
  * - 3. bind function: allow chaining of operations on a monadic value
  * - Implement a fake DOM library
  * - Implement style function
@@ -19,5 +19,32 @@
  *   .style({ color: "red" })
  *   .fadeOut();
  */
+ class Monad {
+    static get [Symbol.species]() {
+      console.log(this)
+      return this;
+    }
+
+    constructor(...extensions) {
+      this.extensions = this.extensions;
+    }
+
+   extend(name, fn) {
+    // name style --> function 
+    console.log(name, fn);
+    console.log(this.extensions);
+    this.extensions[name] = fn;
+    console.log(this.constructor)
+    // return this.constructor;
+    const This = this.constructor[Symbol.species]; // function Monad
+    return new This(name, fn);
+   }
+ }
+
 export function d() {
+  return new Monad({});
 }
+
+// lift - which converts a ‘simple’ function into a debuggable function
+// bind - which converts a debuggable function into a composable form
+// unit - which converts a simple value into the format required for debugging, by placing it in a containe
