7eef45bb248e6362f6d05b14a75a7813936338ff
Hannes Moser <hannes@impossiblearts.com>
2018-05-08 00:26:09 +0200

    docs: improve description

--- 03-router/src/router.js
+++ 03-router/src/router.js
@@ -1,18 +1,30 @@
 /**
- * Implement a depenerouter for web browsers
+ * Implement a dependency free global router for web browsers
+ *
+ * - It should allow static paths
+ * - It should invoke function for "/" if defined on start
+ * - It should have WILDCARD support * for catch all route
+ * - It should never fail (provide error fallback)
+ * - It should allow static redirects
  *
  * API:
  *
- * - page(path, function)
+ * Static:
  * - page('/', index)
+ *
+ * Dynamic:
  * - page('/user/:user', show)
  * - page('/user/:user/edit', edit)
  * - page('/user/:user/album', album)
  * - page('/user/:user/album/sort', sort)
+ *
+ * Redirects:
+ * - page('/home', index)
+ * - page('/', '/home')
+ *
+ * Catch all:
  * - page('*', notfound)
+ *
+ * Start:
  * - page()
  */
-
-export default function router(path, fn) {
-
-}
